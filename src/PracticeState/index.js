21518740330b9704baf23fef83165665c6596bfa
lieutranhieuhoai-coder <lieutranhieuhoai@gmail.com>
2024-04-09 13:06:42 +0700

    new

--- src/PracticeState/index.js
+++ src/PracticeState/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ListGlass from './ListGlass'
+import Layout from '../BootstrapLayout'
 export default class PracticeState extends Component {
   render() {
     return (
       <div>
+{/* <Layout/> */}
         <ListGlass/>
       </div>
     )
